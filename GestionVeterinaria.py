d1609bc9e0bc51c83aa41098afb917ef106f70d8
sergio-programming <sergio0310@live.com>
2025-01-23 00:00:59 -0500

    Implementacion de la funcion actualizarCliente

--- GestionVeterinaria.py
+++ GestionVeterinaria.py
@@ -63,7 +63,31 @@ class Cliente(Persona):
         Mascotas: {self._mascotas}
         """)
         
+class Veterinario(Persona):
+    def __init__(self, id, nombre, telefono, direccion, especialidad):
+        super().__init__(id, nombre, telefono, direccion) 
+        self._especialidad = especialidad
+        
+    def mostrarInformacion(self):
+        return dedent(f"""
+        Nombre del veterinario: {self._nombre}
+        Telefono: {self._telefono}
+        Direccion: {self._direccion}
+        Especialidad: {self._especialidad}
+        """)
+        
+class RegistroMascota(Mascota):
+    def agregarAlHistorial(self, detallesServicio):
+        self.historialCitas.append(detallesServicio)
+        
+    def obtenerHistorial(self):
+        return self.historialCitas
     
+class CitaMascota(Cita):
+    def actualizar(self, **kwargs):
+        for clave, valor in kwargs.items():
+            if hasattr(self, clave):
+                setattr(self, clave, valor)          
 
 class GestionVeterinaria:
     def __init__(self):
@@ -74,20 +98,66 @@ class GestionVeterinaria:
         
     def registrarCliente(self):
         while True:
+            print("#"*30)
+            print("MODULO DE REGISTRO DE CLIENTE")
+            print("#"*30)
             try:
                 id = len(self.clientes) + 1
-                nombreCliente = input("Ingrese el nombre del cliente: ").strip()
-                telefonoCliente = input("Ingrese el telefono del cliente: ").strip()
-                direccionCliente = input("Ingrese el nombre del cliente: ").strip()
+                nombre = input("\nIngrese el nombre del cliente: ").strip()
+                telefono = input("Ingrese el telefono del cliente: ").strip()
+                direccion = input("Ingrese la d del cliente: ").strip()
             
-                if nombreCliente is None or telefonoCliente is None or direccionCliente is None:
-                    raise ValueError("Todos los campos son obligatorios. Por favor intente de nuevo")
+                if nombre is None or telefono is None or direccion is None:
+                    raise ValueError("\nTodos los campos son obligatorios. Por favor intente de nuevo")
 
-                cliente = Cliente(id, nombreCliente, telefonoCliente, direccionCliente)
+                cliente = Cliente(id, nombre, telefono, direccion)
                 self.clientes.append(cliente)
+                print("Cliente registrado exitosamente")
+                input("Presione <Enter> para continuar")
+                break
                 
             except ValueError as e:
                 print(f"Error: {e}")
                 input("Presione <Enter> para continuar")
             
-        
+    def actualizarCliente(self):
+        cliente_a_modificar = None
+        while True:
+            print("#"*30)
+            print("MODULO DE ACTUALIZACIÓN DE CLIENTE")
+            print("#"*30)
+            try:
+                while True:
+                    try:
+                        id = int(input("\nIngrese el número de id del cliente: "))
+                        for cliente in self.clientes:
+                            if cliente.id == id:
+                                cliente_a_modificar = cliente
+                                break
+                        
+                        if cliente_a_modificar:
+                            nuevoNombre = input("Ingrese el nombre del cliente: ").strip()
+                            if nuevoNombre is None:
+                                nuevoNombre = cliente_a_modificar._nombre
+                                    
+                            nuevoTelefono = input("Ingrese el telefono del cliente: ").strip()
+                            if nuevoTelefono is None:
+                                nuevoTelefono = cliente_a_modificar._telefono
+                                    
+                            nuevaDireccion = input("Ingrese la dirección del cliente: ").strip()
+                            if nuevaDireccion is None:
+                                nuevaDireccion = cliente_a_modificar._direccion
+                                                              
+                            
+                            
+                        else:
+                            print("\nNo existe un cliente con el ID ingresado. Por favor intente de nuevo.")
+                            input("Presione <Enter> para continuar")
+                    except ValueError:
+                        print("\nDebe ingresar un número valido. Por favor intente de nuevo.")
+                        input("Presione <Enter> para continuar")
+                        
+                
+                
+            except print(0):
+                pass
