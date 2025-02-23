a97793a2ffd2f93c790ca48faf64441b4aabd99c
sergio-programming <sergio0310@live.com>
2025-02-22 23:02:05 -0500

    Implementacion de las funciones registrarMascota y programarCita

--- GestionVeterinaria.py
+++ GestionVeterinaria.py
@@ -49,22 +49,25 @@ class Cliente(Persona):
                    
         
     def mostrarInformacion(self):
-        return dedent(f"""
+        infoCliente = dedent(f"""
         Nombre del cliente: {self._nombre}
         Telefono: {self._telefono}
         Direccion: {self._direccion}
         """)
+        
         if not self._mascotas:
-            print("El cliente por el momento no tiene mascostas registradas")
+            infoCliente += "\nEl cliente por el momento no tiene mascostas registradas"
         else:
             for i, mascota in enumerate(self._mascotas, start=1):
-                return dedent(f"""
+                infoCliente += dedent(f"""
                 Mascota # {i}
-                Nombre de la mascota: {mascota.nombre}
-                Especie: {mascota.especie}
-                Raza: {mascota.raza}
-                edad: {mascota.edad}
+                Nombre de la mascota: {mascota._nombre}
+                Especie: {mascota._especie}
+                Raza: {mascota._raza}
+                Edad: {mascota._edad}
                 """)
+        
+        return infoCliente
         
 class Veterinario(Persona):
     def __init__(self, id, nombre, telefono, direccion, especialidad):
@@ -80,8 +83,8 @@ class Veterinario(Persona):
         """)
         
 class GestionMascota(Mascota):
-    def agregarAlHistorial(self, detallesServicio):
-        self._historialCitas.append(detallesServicio)
+    def agregarAlHistorial(self, citaProgramada):
+        self._historialCitas.append(citaProgramada)
         
     def obtenerHistorial(self):
         return self._historialCitas
@@ -114,12 +117,12 @@ class GestionVeterinaria:
 
                 cliente = Cliente(id, nombre, telefono, direccion)
                 self.clientes.append(cliente)
-                print("Cliente registrado exitosamente.")
+                print("\nCliente registrado exitosamente.")
                 input("Presione <Enter> para continuar")
                 break
                 
             except ValueError as e:
-                print(f"Error: {e}")
+                print(f"\nError: {e}")
                 input("Presione <Enter> para continuar")
             
     def actualizarCliente(self):
@@ -144,7 +147,7 @@ class GestionVeterinaria:
                     break
                 
             if cliente_a_modificar is None:
-                print(f"\nNo existe un cliente con el ID {idCliente}. Por favor intente nuevamente.")
+                print(f"\nNo existe un cliente con el ID {idCliente}. Por favor intente de nuevo.")
                 continue
 
             nuevoNombre = input("\nPor favor actualice el nombre: ").strip()
@@ -160,7 +163,7 @@ class GestionVeterinaria:
                 nuevaDireccion = cliente_a_modificar._direccion
                     
             self.clientes[indice] = Cliente(idCliente, nuevoNombre, nuevoTelefono, nuevaDireccion)
-            print("Cliente actualizado exitosamente.") 
+            print("\nCliente actualizado exitosamente.") 
             input("Presione <Enter> para continuar")
             break  
             
@@ -183,7 +186,7 @@ class GestionVeterinaria:
 
                 veterinario = Veterinario(id, nombre, telefono, direccion, especialidad)
                 self.veterinarios.append(veterinario)
-                print("Cliente registrado exitosamente.")
+                print("\nVeterinario registrado exitosamente.")
                 input("Presione <Enter> para continuar")
                 break
                 
@@ -213,7 +216,7 @@ class GestionVeterinaria:
                     break
                 
             if veterinario_a_modificar is None:
-                print(f"\nNo existe un cliente con el ID {idVeterinario}. Por favor intente nuevamente.")
+                print(f"\nNo existe un veterinario con el ID {idVeterinario}. Por favor intente de nuevo.")
                 continue
 
             nuevoNombre = input("\nPor favor actualice el nombre: ").strip()
@@ -233,9 +236,103 @@ class GestionVeterinaria:
                 nuevaEspecialidad = veterinario_a_modificar._especialidad
                     
             self.veterinarios[indice] = Veterinario(idVeterinario, nuevoNombre, nuevoTelefono, nuevaDireccion, nuevaEspecialidad)
-            print("Veterinario actualizado exitosamente.") 
+            print("\nVeterinario actualizado exitosamente.") 
             input("Presione <Enter> para continuar")
             break
         
+    def registrarMascota(self):
+        while True:
+            print()
+            print("#"*30)
+            print("MODULO DE REGISTRO DE MASCOTA")
+            print("#"*30)
+            
+            try:
+                idCliente = int(input("Ingrese el id del cliente dueño de la mascota: ").strip())
+                cliente = next((c for c in self.clientes if idCliente == c._id), None)
+              
+                if not cliente:
+                    raise ValueError(f"\nNo existe un cliente con el ID {idCliente}. Por favor intente nuevamente.")
+
+                while True:
+                    try:
+                        nroMascotas = int(input("¿Cuantas mascotas desea registrar? (Puede registrar maximo 3 por intento): ").strip())
+                        if nroMascotas <= 0 or nroMascotas > 3:
+                            raise ValueError(f"El número ingresado no es valido. Por favor intente de nuevo.")
+                        break
+                    except ValueError as e:
+                        print(f"Error: {e}")            
+
+                for i in range(nroMascotas):
+                    print(f"\nRegistre la mascota #{i+1}")
+                    while True:
+                        try:
+                            nombre = input("Ingrese el nombre de la mascota: ").strip()
+                            especie = input("Ingrese la especie de la mascota: ").strip()
+                            raza = input("Ingrese la raza de la mascota: ").strip()
+                            edad = int(input("Ingrese la edad de la mascota: ").strip())
+                            
+                            if not nombre or not especie or not raza or edad < 0:
+                                raise ValueError("Todos los campos son obligatorios. Por favor intente de nuevo.")
+                            break
+                        except ValueError as e:
+                            print(f"Error: {e}")
+                    
+                    mascota = GestionMascota(nombre, especie, raza, edad)
+                    cliente.agregarMascota(mascota)
+                    
+                print(f"\n{'Mascota registrada exitosamente.' if nroMascotas == 1 else 'Mascotas registradas exitosamente.'}")
+                input("Presione <Enter> para continuar")
+                break
+                
+              
+            except ValueError as e:
+                print(f"\nError: {e}")
+                input("Presione <Enter> para continuar")
+              
+    def programarCita(self):
+        try:
+            idCliente = int(input("Ingrese el id del cliente dueño de la mascota: ").strip())
+            nombreMascota = input("Ingrese el nombre de la mascota: ").strip()
+            
+            cliente = next((c for c in self.clientes if c._id == idCliente), None)
+            if not cliente:
+                raise ValueError("Cliente no registrado. Por favor intente de nuevo.")
+            
+            mascota = next((m for m in self.cliente.mascotas if m.nombre == nombreMascota), None)
+            if not mascota:
+                raise ValueError("Mascota no registrada. Por favor intente de nuevo.")
+            
+            while True:
+                try:
+                    fecha = input("\nIngrese la fecha de la cita (AAAA-MM-DD): ").strip()
+                    hora = input("Ingrese la hora de la cita (HH:MM): ").strip()
+                    servicio = input("Ingrese el servicio deseado (Consulta, Vacunación, Cirugia, etc): ").strip()
+                    idVeterinario = int(input("Ingrese el id del veterinario a agendar: ").strip())
+                    
+                    veterinario = next((v for v in self.veterinarios if v._id == idVeterinario), None)
+                    
+                    if not fecha or not hora or not servicio or not veterinario:
+                        raise ValueError("Todos los campos son obligatorios. Por favor intente de nuevo.")
+                    
+                    if not veterinario:
+                        raise ValueError(f"No existe un veterinario con el ID {idVeterinario}. Por favor intente de nuevo.")
+                    
+                    if servicio != veterinario._especialidad:
+                        raise ValueError(f"El veterinario {veterinario._nombre} no atiende el servicio seleccionado. Por favor intente de nuevo.")
+                    
+                    break
+                except ValueError as e:
+                    print(f"\nError: {e}")
+            
+            
+            
+            datetime.strptime(fecha, "%Y-%m-%d")
+            datetime.strptime(hora, "%H:%M")
+            
+            cita = GestionMascota(fecha, hora, servicio, veterinario)
+            mascota.agregarAlHistorial(cita)
+            print("\n¡Cita agregada exitosamente!")
         
-        
+        except ValueError as e:
+            print(f"\nError: {e}")
---
+++ menu.py
@@ -0,0 +1,9 @@
+from GestionVeterinaria import GestionVeterinaria
+
+def menu():
+    print()
+    print("#"*30)
+    print("BIENVENIDO A LA PLATAFORMA DE HUELLA FELIZ")
+    print("#"*30)
+    
+    print (dedent(f""))
